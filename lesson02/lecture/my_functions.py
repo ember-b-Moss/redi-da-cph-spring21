63ccd30f6f93d0e46aaa98fddc1925267eb5ce82
Petter Becker-Jostes <petterbj@outlook.dk>
2021-04-11 20:23:07 +0200

    Add explanation and scripts about file import.

---
+++ lesson02/lecture/my_functions.py
@@ -0,0 +1,16 @@
+"""
+Python script to show how file import works with functions.
+"""
+
+def multiply_2():
+    """
+    According to PEP8, there has to be a docstring here to explain
+    the function, but it's pretty obvious.
+    """ 
+    return 2 + 2
+
+def add_50(x):
+    """
+    Adds 50 to the input value x.
+    """
+    return x + 50
